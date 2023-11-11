3679f048523aa0b87679c28ab4c4bc6a9770b0de
vkele12 <104184698+vkele12@users.noreply.github.com>
2023-11-11 15:59:16 +0530

    Update main.tf

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example" {
   ami           = "ami-0287a05f0ef0e9d9a"
   instance_type = "t2.micro"
 
- # tags = {
- #   Name = "var.ec2_name"
- # }
+  tags = {
+    Name = "var.ec2_name"
+  }
 }
